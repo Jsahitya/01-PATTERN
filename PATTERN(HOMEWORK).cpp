9d0f37333a08760edb671d4e5f597a3ab8bcb927
Sahitya Jain <63090837+Jsahitya@users.noreply.github.com>
2020-05-14 18:10:23 +0530

    Add files via upload

---
+++ PATTERN(HOMEWORK).cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main()
+{
+	int i=0;
+	int o=0;
+	for(i=1;i<=6;i++)
+	{
+		printf("01\n",i);
+		for(o=1;o<=i;o++)
+		{
+			printf("01\t",o);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
